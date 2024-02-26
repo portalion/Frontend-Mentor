649e40d089c7289d2af8ae92c17bd636875592f2
Portalion <77tobik77@gmail.com>
2024-02-26 19:53:19 +0100

    add first tests

---
+++ tests/FrontentMentorBlazorSolutionTests/MainTests.cs
@@ -0,0 +1,12 @@
+namespace FrontentMentorBlazorSolutionTests
+{
+    [TestClass]
+    public class MainTests
+    {
+        [TestMethod]
+        public void TestIfTestsAreWorking()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
