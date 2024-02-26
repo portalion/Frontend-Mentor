d451cda9d07a58814d085e299f259a38628bd04d
Portalion <77tobik77@gmail.com>
2024-02-26 01:03:19 +0100

    add webscrappingpart of frontend mentor readme updater

---
+++ Tools/Frontend Mentor Readme Updater/FrontendMentor Readme Updater/FrontendMentorParser.cs
@@ -0,0 +1,57 @@
+﻿using AngleSharp;
+
+namespace FrontendMentor_Readme_Updater;
+
+public struct FrontendMentorChallengeData
+{
+    public string Title;
+    public string Difficulty;
+    public bool Premium;
+}
+
+public class FrontendMentorParser
+{
+    readonly IConfiguration config;
+    readonly IBrowsingContext context;
+
+    public FrontendMentorParser()
+    {
+        config = Configuration.Default;
+        context = BrowsingContext.New(config);
+    }
+
+    public async Task<List<FrontendMentorChallengeData>> ParseHtml(string source)
+    {
+        var result = new List<FrontendMentorChallengeData>();
+
+        var document = await context.OpenAsync(req => req.Content(source));
+        var cards = document.QuerySelectorAll("div").Where(a => a?.ClassName?.Contains("Card__Wrapper") ?? false);
+        foreach(var card in cards)
+        {
+            var toAppend = new FrontendMentorChallengeData();
+
+            try
+            {
+                var divWithNeededData = card.ChildNodes[2];
+                var dificultyDiv = divWithNeededData?.ChildNodes[1].ChildNodes[1].FirstChild;
+
+                var difficultyNumber = dificultyDiv?.ChildNodes[0].TextContent ?? string.Empty;
+                var difficultyText = dificultyDiv?.ChildNodes[1].TextContent ?? string.Empty;
+
+                toAppend.Title = divWithNeededData?.FirstChild?.FirstChild?.TextContent ?? string.Empty;
+                
+                if(string.IsNullOrEmpty(toAppend.Title) || 
+                    string.IsNullOrEmpty(difficultyText) ||
+                    string.IsNullOrEmpty(difficultyNumber))
+                {
+                    continue;
+                }
+                toAppend.Difficulty = $"{difficultyNumber} - {difficultyText}";
+
+                result.Add(toAppend);
+            }
+            catch { }
+        }
+        return result;
+    }
+}
---
+++ Tools/Frontend Mentor Readme Updater/FrontendMentor Readme Updater/Program.cs
@@ -0,0 +1,25 @@
+﻿using FrontendMentor_Readme_Updater;
+
+HttpClient httpClient = new HttpClient();
+const string url = "https://www.frontendmentor.io/challenges";
+
+try
+{
+    FrontendMentorReader reader = new(httpClient);
+    FrontendMentorParser parser = new FrontendMentorParser();
+
+    var website = await reader.ReadUrl(url);
+    var listValues = await parser.ParseHtml(website);
+    var cleanedData = FrontendMentorPrettier.SortAndAssignData(listValues);
+    var result = cleanedData.OrderBy(x => x.Key);
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine("Exception thrown when trying to get http response");
+    Console.WriteLine($"Message: {e.Message}");
+}
+catch (Exception e)
+{
+    Console.WriteLine("Exception thrown");
+    Console.WriteLine($"Message: {e.Message}");
+}
